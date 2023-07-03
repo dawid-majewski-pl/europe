7eeda951dba7f4d6728cd18b0d932e3cd572304e
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-03 08:05:12 +0200

    Added cards to Countries section, fixed navbar z-index.

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -19,7 +19,7 @@ const Countries = () => {
                     <div class='flex flex-col items-center'>
                         <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Spain</h3>
                         <p className='text-gray-700 pb-2'>Hola, Barcelona!</p>
-                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit UK</button>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Spain</button>
                     </div>
                 </div>
                 <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
@@ -27,7 +27,7 @@ const Countries = () => {
                     <div class='flex flex-col items-center'>
                         <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Norway</h3>
                         <p className='text-gray-700 pb-2'>Welcome to cold and nice Oslo</p>
-                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit UK</button>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Norway</button>
                     </div>
                 </div>
                 <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
@@ -35,7 +35,39 @@ const Countries = () => {
                     <div class='flex flex-col items-center'>
                         <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Ireland</h3>
                         <p className='text-gray-700 pb-2'>Calm green place</p>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Ireland</button>
+                    </div>
+                </div>
+                <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
+                    <img className='w-full' src={London} alt='/' />
+                    <div class='flex flex-col items-center'>
+                        <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Germany</h3>
+                        <p className='text-gray-700 pb-2'>Let's start from London</p>
                         <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit UK</button>
+                    </div>
+                </div>
+                <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
+                    <img className='w-full' src={London} alt='/' />
+                    <div class='flex flex-col items-center'>
+                        <h3 className='text-2xl font-bold text-center pt-4 pb-2'>France</h3>
+                        <p className='text-gray-700 pb-2'>Hola, Barcelona!</p>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Spain</button>
+                    </div>
+                </div>
+                <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
+                    <img className='w-full' src={London} alt='/' />
+                    <div class='flex flex-col items-center'>
+                        <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Sweeden</h3>
+                        <p className='text-gray-700 pb-2'>Welcome to cold and nice Oslo</p>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Norway</button>
+                    </div>
+                </div>
+                <div className='h-fit bg-white w-full border rounded-xl overflow-hidden'>
+                    <img className='w-full' src={London} alt='/' />
+                    <div class='flex flex-col items-center'>
+                        <h3 className='text-2xl font-bold text-center pt-4 pb-2'>Greece</h3>
+                        <p className='text-gray-700 pb-2'>Calm green place</p>
+                        <button className='bg-green-400 text-black w-[200px] uppercase rounded-md font-medium my-6 py-3 duration-300 top-0 hover:scale-110 relative hover:top-2'>Visit Ireland</button>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const handleNav = () => {
             <div onClick={handleNav} className='block md:hidden'>
                 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-slate-950 bg-opacity-90 ease-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
+            <div className={!nav ? 'fixed z-[30] left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-slate-950 bg-opacity-90 ease-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
                 <h2 className='w-full text-3xl font-bold text-green-400 mx-4 my-8'>Europe</h2>
                 <ul className='uppercase'>
                     <li className='p-4 border-b border-b-gray-800'>Home</li>
